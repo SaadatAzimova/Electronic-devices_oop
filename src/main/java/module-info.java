272e0bd4a83d1cfff25ac90353ff2ef67c2071d0
SaadatAzimova <saadatazimova5@gmail.com>
2024-10-25 22:08:15 +0600

    add buttons and text fields

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.electronicdevices {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens org.example.electronicdevices to javafx.fxml;
--- src/main/java/org/example/electronicdevices/HelloController.java
+++ src/main/java/org/example/electronicdevices/HelloController.java
@@ -1,8 +1,103 @@
 package org.example.electronicdevices;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
+    @FXML
+    private ToggleGroup Choise;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField lCameraResolution;
+
+    @FXML
+    private TextField lName;
+
+    @FXML
+    private TextField lPrice;
+
+    @FXML
+    private TextField lRamSize;
+
+    @FXML
+    private TextField lWeight;
+
+    @FXML
+    private RadioButton rbL;
+
+    @FXML
+    private RadioButton rbS;
+
+    @FXML
+    private RadioButton rbT;
+
+    @FXML
+    private Button removeButton;
+
+    @FXML
+    private TextField sName;
+
+    @FXML
+    private TextField sPrice;
+
+    @FXML
+    private TextField sResolution;
+
+    @FXML
+    private TextField sScreenSize;
+
+    @FXML
+    private TextField sWeight;
+
+    @FXML
+    private TextField tBatteryLife;
+
+    @FXML
+    private TextField tName;
+
+    @FXML
+    private TextField tPrice;
+
+    @FXML
+    private TextField tProcessorType;
+
+    @FXML
+    private TextField tWeight;
+
+    @FXML
+    void onAddClick() {
+
+    }
+
+    @FXML
+    void onCancelClick() {
+
+    }
+
+    @FXML
+    void onRbLClick() {
+
+    }
+
+    @FXML
+    void onRbSClick() {
+
+    }
+
+    @FXML
+    void onRbTClick() {
+
+    }
+
+    @FXML
+    void onRemoveClick() {
+
+    }
 
 }
